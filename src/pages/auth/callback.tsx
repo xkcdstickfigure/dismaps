c9fb8beb3596b055cadf354c75dee06fe72c0b0c
Archie Baer <archie@abaer.dev>
2023-02-25 22:42:24 +0000

    prisma

--- src/pages/auth/callback.tsx
+++ src/pages/auth/callback.tsx
@@ -1,5 +1,6 @@
 import { GetServerSideProps } from "next"
 import { getProfile } from "@/lib/discord"
+import db, { prisma } from "@/lib/prisma"
 
 export default () => null
 
@@ -16,8 +17,36 @@ export const getServerSideProps: GetServerSideProps = async ({
 	if (typeof code !== "string") return redirectHome
 
 	try {
+		// get profile from code
 		let { profile, tokens } = await getProfile(code)
-		console.log(`${profile.username}#${profile.discriminator}`)
+
+		// create user
+		let user = await db.user.upsert({
+			where: {
+				id: profile.id,
+			},
+			create: {
+				id: profile.id,
+				username: profile.username,
+				discriminator: profile.discriminator,
+				avatar: profile.avatar,
+				email: profile.email,
+				emailVerified: profile.verified,
+				accessToken: tokens.access_token,
+				refreshToken: tokens.refresh_token,
+			},
+			update: {
+				username: profile.username,
+				discriminator: profile.discriminator,
+				avatar: profile.avatar,
+				email: profile.email,
+				emailVerified: profile.verified,
+				accessToken: tokens.access_token,
+				refreshToken: tokens.refresh_token,
+			},
+		})
+
+		console.log(user)
 	} catch (err) {}
 
 	return redirectHome
