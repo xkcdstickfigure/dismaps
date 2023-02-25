2663522f6fd627dfbc15e0b9062a442fb495a9dd
Archie Baer <archie@abaer.dev>
2023-02-25 12:27:20 +0000

    header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default () => (
 	<Html lang="en">
 		<Head />
-		<body>
+		<body className="text-white" style={{ background: "#262626" }}>
 			<Main />
 			<NextScript />
 		</body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,9 @@
-import Head from "next/head";
+import { Layout } from "@/components/Layout";
 
 export default function Home() {
 	return (
-		<>
-			<Head>
-				<title>Dismaps</title>
-				<meta
-					name="description"
-					content="Find Discord servers with people near you!"
-				/>
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-			</Head>
-
+		<Layout>
 			<p>hello</p>
-		</>
+		</Layout>
 	);
 }
