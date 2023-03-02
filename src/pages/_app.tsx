28ecb71ad8492795a20638a1fba381c04f494bd3
Archie Baer <archie@abaer.dev>
2023-03-02 17:34:12 +0000

    added nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/index.css"
 import { AppProps } from "next/app"
+import Router from "next/router"
+import NProgress from "nprogress"
 
 export default ({ Component, pageProps }: AppProps) => (
 	<Component {...pageProps} />
 )
+
+Router.events.on("routeChangeStart", () => NProgress.start())
+Router.events.on("routeChangeComplete", () => NProgress.done())
+Router.events.on("routeChangeError", () => NProgress.done())
