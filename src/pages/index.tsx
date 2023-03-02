56ff3bff3ea027a08006d3c32e79171c7c22da13
Archie Baer <archie@abaer.dev>
2023-03-02 15:26:17 +0000

    shows message when no guilds nearby

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,15 @@ import {
 	MapPin as PinIcon,
 	Users as PopularIcon,
 	Clock as RecentIcon,
+	Map as MapIcon,
+	Plus as PlusIcon,
 } from "react-feather"
 import { GetServerSideProps } from "next"
 import { auth } from "@/lib/auth"
 import { user } from "@/types/user"
 import { square } from "@/lib/coords"
 import db from "@/lib/prisma"
+import Link from "next/link"
 
 const distances = [10, 20, 50]
 
@@ -63,11 +66,45 @@ export default function Page({ user, distance, sortNew, guilds = [] }: Props) {
 				</div>
 
 				{user ? (
-					<div className="space-y-2">
-						{guilds.map((g) => (
-							<Guild key={g.id} {...g} />
-						))}
-					</div>
+					guilds.length > 0 ? (
+						<div className="space-y-2">
+							{guilds.map((g) => (
+								<Guild key={g.id} {...g} />
+							))}
+						</div>
+					) : (
+						<div className="space-y-4 text-center">
+							<MapIcon
+								className="w-48 h-48 text-neutral-400 mx-auto"
+								strokeWidth={1.5}
+							/>
+
+							<div className="space-y-1">
+								<p className="text-2xl font-semibold">
+									You're in uncharted territory!
+								</p>
+								<p className="text-sm text-neutral-400">
+									There aren't any servers nearby. Increase your search
+									distance, or add your own.
+								</p>
+							</div>
+
+							<div className="flex justify-center">
+								<Link
+									href="/add"
+									className={
+										"bg-neutral-700/50 hover:bg-neutral-700 rounded-md flex items-center space-x-1 py-1 pl-2 pr-4"
+									}
+								>
+									<PlusIcon
+										className="w-8 h-8 text-red-500"
+										strokeWidth={1.5}
+									/>
+									<p className="text-lg">Add Server</p>
+								</Link>
+							</div>
+						</div>
+					)
 				) : (
 					<p>You'll need to sign in to see nearby communities</p>
 				)}
