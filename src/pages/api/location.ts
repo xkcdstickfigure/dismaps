ae520f044a1a4b02c98a6901d95ed662512b1d0a
Archie Baer <archie@abaer.dev>
2023-03-02 14:47:23 +0000

    limits location updates

--- src/pages/api/location.ts
+++ src/pages/api/location.ts
@@ -12,6 +12,18 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 	if (typeof lat !== "number" || typeof lon !== "number")
 		return res.status(400).send("invalid body")
 
+	// count location updates
+	// prevent more than 12 in 6 hours
+	let count = await db.location.count({
+		where: {
+			userId: user.id,
+			createdAt: {
+				gt: new Date(new Date().getTime() - 1000 * 60 * 60 * 6),
+			},
+		},
+	})
+	if (count >= 12) return res.json({})
+
 	// create location
 	await db.location.create({
 		data: {
